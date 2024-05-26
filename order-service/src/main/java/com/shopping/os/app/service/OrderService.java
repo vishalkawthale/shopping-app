a3edfcd7c384be8659ecb0fdfad7acf27bbd99d5
vishal_kawthale <vishalkawthale17@gmail.com>
2024-05-26 21:33:34 +0530

    updated config server files

--- order-service/src/main/java/com/shopping/os/app/service/OrderService.java
+++ order-service/src/main/java/com/shopping/os/app/service/OrderService.java
@@ -6,17 +6,23 @@ import com.shopping.os.app.common.TransactionResponse;
 import com.shopping.os.app.entity.Order;
 import com.shopping.os.app.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
-
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String paymentUrl;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
         Order order = request.getOrder();
@@ -25,7 +31,7 @@ public class OrderService {
                 .price(order.getPrice())
                 .build();
 
-        Payment paymentResponse = restTemplate.postForObject("http://payment-service/payment/processPayment", payment, Payment.class);
+        Payment paymentResponse = restTemplate.postForObject(paymentUrl, payment, Payment.class);
         orderRepository.save(order);
         return TransactionResponse.builder()
                 .order(order)
